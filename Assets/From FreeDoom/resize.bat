93d807afe324ad66b8da534fc5d0053ce7017e8c
Andre Baptista <albs_br@yahoo.com.br>
2026-02-19 13:58:17 -0300

    v.0.10.1

--- Assets/From FreeDoom/resize.bat
+++
@@ -1,1 +0,0 @@
-magick *.png -resize 40% resized_%03d.png
---
+++ Assets/From FreeDoom/shotgun single and double/resize.bat
@@ -0,0 +1,21 @@
+# magick *.png -resize 40% resized_%03d.png
+
+# +dither is used to disable dithering
+# double percent used to escape single percent
+
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2a0.png -filter point -resize 40%% resized_sht2a0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2b0.png -filter point -resize 40%% resized_sht2b0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2c0.png -filter point -resize 40%% resized_sht2c0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2d0.png -filter point -resize 40%% resized_sht2d0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2e0.png -filter point -resize 40%% resized_sht2e0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2f0.png -filter point -resize 40%% resized_sht2f0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2g0.png -filter point -resize 40%% resized_sht2g0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2h0.png -filter point -resize 40%% resized_sht2h0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2i0.png -filter point -resize 40%% resized_sht2i0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2j0.png -filter point -resize 40%% resized_sht2j0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick shtfa0.png -filter point -resize 40%% resized_shtfa0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick shtfb0.png -filter point -resize 40%% resized_shtfb0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick shtga0.png -filter point -resize 40%% resized_shtga0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick shtgb0.png -filter point -resize 40%% resized_shtgb0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick shtgc0.png -filter point -resize 40%% resized_shtgc0.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick shtgd0.png -filter point -resize 40%% resized_shtgd0.png
