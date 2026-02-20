11787f8c50013360378d157b87c6ce5437da21f5
Andre Baptista <albs_br@yahoo.com.br>
2026-02-20 12:22:02 -0300

    v.0.10.2

---
+++ Assets/From FreeDoom/enemy cpos/resize.bat
@@ -0,0 +1,56 @@
+# +dither is used to disable dithering
+# double percent used to escape single percent
+
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick sht2a0.png -filter point -resize 40%% resized_sht2a0.png
+
+
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposa1.png   -filter point -resize 40%% resized/resized_cposa1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposa2a8.png -filter point -resize 40%% resized/resized_cposa2a8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposa3a7.png -filter point -resize 40%% resized/resized_cposa3a7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposa4a6.png -filter point -resize 40%% resized/resized_cposa4a6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposa5.png   -filter point -resize 40%% resized/resized_cposa5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposb1.png   -filter point -resize 40%% resized/resized_cposb1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposb2b8.png -filter point -resize 40%% resized/resized_cposb2b8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposb3b7.png -filter point -resize 40%% resized/resized_cposb3b7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposb4b6.png -filter point -resize 40%% resized/resized_cposb4b6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposb5.png   -filter point -resize 40%% resized/resized_cposb5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposc1.png   -filter point -resize 40%% resized/resized_cposc1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposc2c8.png -filter point -resize 40%% resized/resized_cposc2c8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposc3c7.png -filter point -resize 40%% resized/resized_cposc3c7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposc4c6.png -filter point -resize 40%% resized/resized_cposc4c6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposc5.png   -filter point -resize 40%% resized/resized_cposc5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposd1.png   -filter point -resize 40%% resized/resized_cposd1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposd2d8.png -filter point -resize 40%% resized/resized_cposd2d8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposd3d7.png -filter point -resize 40%% resized/resized_cposd3d7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposd4d6.png -filter point -resize 40%% resized/resized_cposd4d6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposd5.png   -filter point -resize 40%% resized/resized_cposd5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cpose1.png   -filter point -resize 40%% resized/resized_cpose1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cpose2e8.png -filter point -resize 40%% resized/resized_cpose2e8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cpose3e7.png -filter point -resize 40%% resized/resized_cpose3e7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cpose4e6.png -filter point -resize 40%% resized/resized_cpose4e6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cpose5.png   -filter point -resize 40%% resized/resized_cpose5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposf1.png   -filter point -resize 40%% resized/resized_cposf1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposf2f8.png -filter point -resize 40%% resized/resized_cposf2f8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposf3f7.png -filter point -resize 40%% resized/resized_cposf3f7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposf4f6.png -filter point -resize 40%% resized/resized_cposf4f6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposf5.png   -filter point -resize 40%% resized/resized_cposf5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposg1.png   -filter point -resize 40%% resized/resized_cposg1.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposg2g8.png -filter point -resize 40%% resized/resized_cposg2g8.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposg3g7.png -filter point -resize 40%% resized/resized_cposg3g7.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposg4g6.png -filter point -resize 40%% resized/resized_cposg4g6.png
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposg5.png   -filter point -resize 40%% resized/resized_cposg5.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposh0.png   -filter point -resize 40%% resized/resized_cposh0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposi0.png   -filter point -resize 40%% resized/resized_cposi0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposj0.png   -filter point -resize 40%% resized/resized_cposj0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposk0.png   -filter point -resize 40%% resized/resized_cposk0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposl0.png   -filter point -resize 40%% resized/resized_cposl0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposm0.png   -filter point -resize 40%% resized/resized_cposm0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposn0.png   -filter point -resize 40%% resized/resized_cposn0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposo0.png   -filter point -resize 40%% resized/resized_cposo0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposp0.png   -filter point -resize 40%% resized/resized_cposp0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposq0.png   -filter point -resize 40%% resized/resized_cposq0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposr0.png   -filter point -resize 40%% resized/resized_cposr0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cposs0.png   -filter point -resize 40%% resized/resized_cposs0.png  
+C:\Users\Public\utils\ImageMagick-7.1.2-13-portable-Q16-x64\magick cpost0.png   -filter point -resize 40%% resized/resized_cpost0.png  
+
+ 
